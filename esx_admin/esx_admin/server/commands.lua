2daa435e79a5a743aada851569e1c8feb37af6ba
ZokuRush <67478346+ZokuRush@users.noreply.github.com>
2022-05-27 20:06:52 -0600

    Delete esx_admin/esx_admin directory

--- esx_admin/esx_admin/server/commands.lua
+++
@@ -1,7 +0,0 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-ESX.RegisterCommand('a', 'admin', function(xPlayer, args, showError)
-    xPlayer.triggerEvent('esx_admin:OpenMenu', args.playerId)
-end, false)
